551d107343baefe273f958f298862810d0f720fd
Michael Sundgaard <sundgaar@amazon.com>
2020-10-22 18:16:59 +0200

    Updated

--- src/main/java/com/opusmagus/controller/TradeController.java
+++ src/main/java/com/opusmagus/controller/TradeController.java
@@ -1,9 +1,12 @@
 package com.opusmagus.controller;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.opusmagus.dto.Trade;
-import org.springframework.web.bind.annotation.*;
-
-import com.opusmagus.dto.Trade;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /** Note that we have annotated the DemoController class with @Controller and @RequestMapping("/welcome"). 
  * When Spring scans our package, it will recognize this bean as being a Controller bean for processing requests. 
@@ -27,7 +30,20 @@ public class TradeController {
 		customer.Gender = "Male";
 		customer.Address = "Mosede Kærvej 37";
 		customer.Id = id;*/
+		PostToTradeQueue();
 		return trade;
+	}
+
+	// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/examples-sqs-visibility-timeout.html
+	public void PostToTradeQueue() {
+
+		AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+		var queueUrl = "tradeQ";
+		SendMessageRequest send_msg_request = new SendMessageRequest()
+				.withQueueUrl(queueUrl)
+				.withMessageBody("hello world")
+				.withDelaySeconds(5);
+		sqs.sendMessage(send_msg_request);
 	}
 
 }
