cee8fa868c708785c1804a2b53573e754a3d1075
Michael Sundgaard <sundgaar@amazon.com>
2020-10-25 09:47:04 +0100

    Fixed after install script

--- sh/after-install.sh
+++ sh/after-install.sh
@@ -1,9 +1,14 @@
+echo Updating trading app linux service definition...
 cp /var/app/trading-app.service /etc/systemd/system/
+echo Reloading linux daemon service...
 systemctl daemon-reload
+echo Fixing compressed JAR...
 cd /var/app
 rm -Rf jar
 mkdir jar
 cd jar
 jar xf ../*.jar
 rm -f ../*.jar
-jar cvf0 ../new.jar *
+echo Creating new uncompressed JAR...
+jar cmvf0 META-INF/MANIFEST.MF ../trading-app-0.0.1-SNAPSHOT.jar *
+echo Done.
