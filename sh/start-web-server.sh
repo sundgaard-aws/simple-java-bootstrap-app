5dd748e86fafddfa260b6453614aede62a10f6be
Michael Sundgaard <sundgaar@amazon.com>
2020-10-31 09:32:19 +0100

    Updated validate service

--- sh/start-web-server.sh
+++ sh/start-web-server.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 echo $PWD
 ls -al
 # Notice that /var/app is configured in appspec.yml and could be any path you want
--- sh/stop-web-server.sh
+++ sh/stop-web-server.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 systemctl stop trading-app.service
